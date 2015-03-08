3b1aab0b6c787caf7f6dcae6f346a3051b177b92
Mauro Scomparin <scompo@gmail.com>
2015-03-08 20:46:57 +0100

    Initial commit

---
+++ src/test/java/it/scompo/FitApp/test/ApplicationTest.java
@@ -0,0 +1,30 @@
+package it.scompo.FitApp.test;
+
+import it.scompo.FitApp.Application;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+public class ApplicationTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+		// empty test, the application should start..
+	}
+
+}
