3556174afcab781cad632e051d22c61128724eb4
Mauro Scomparin <scompo@gmail.com>
2015-03-17 23:02:29 +0100

    fixed tests

--- src/test/resources/public/js/ActivitiesControllerTest.js
+++ src/test/resources/public/js/ActivitiesControllerTest.js
@@ -1,12 +1,19 @@
 describe('Activities controller test', function() {
 
 	beforeEach(module('activitiesMain.controllers'));
+	beforeEach(module('activitiesMain.services'));
 
 	var $controller;
+	var $httpBackend;
 
-	beforeEach(inject(function(_$controller_) {
+	beforeEach(inject(function(_$controller_, _$httpBackend_) {
 
 		$controller = _$controller_;
+		$httpBackend = _$httpBackend_;
+		$httpBackend.expectGET(/^\/api\/v1\/activities\?.*/).respond({
+			totalPages : 1,
+			content : []
+		});
 	}));
 
 	describe('Controller new data', function() {
@@ -17,33 +24,26 @@ describe('Activities controller test', function() {
 
 			$scope = {};
 			controller = $controller('activitiesController', {
-				
-				$scope : $scope,
-				
-				activitiesRestService : {
-					getPaginated : function() {
-						return {
-							totalPages : 0,
-							content : []
-						};
-					}
-				}
+
+				$scope : $scope
 			});
 		});
 
 		it('should create a new instance to save', function() {
-
+		
 			expect($scope.newItem).not.toBe(null);
 		});
 
 		it('should create activities', function() {
-
+			
+			$httpBackend.flush();	
 			expect($scope.activities).toEqual([]);
 		});
 
 		it('should get the number of pages', function() {
-
-			expect($scope.totalPages).toBe(0);
+			
+			$httpBackend.flush();
+			expect($scope.totalPages).toBe(1);
 		});
 	});
 });
